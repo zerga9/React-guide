414a155295d3520df672360deaf490fc9fd47810
Gadiza Zerari <gadizaz@hotmail.com>
2018-09-05 16:01:35 +0100

    Using props to change DOM on click

--- src/App.js
+++ src/App.js
@@ -28,9 +28,16 @@ class App extends Component {
         <h1> Hi, Im a React App</h1>
         <p>This is really working</p>
         <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <Person
+        name={this.state.persons[0].name}
+        age={this.state.persons[0].age} />
+        <Person
+         name={this.state.persons[1].name}
+         age={this.state.persons[1].age}
+         click={this.switchNameHandler} >My Hobbies: Racing</Person>
+        <Person
+        name={this.state.persons[2].name} 
+        age={this.state.persons[2].age}/>
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'))
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,13 +1,12 @@
 import React from 'react';
 
-const person = (props) => {
-
+const person = ( props ) => {
   return (
     <div>
-      <p>I'm {props.name} and I am {props.age}  years old!!</p>
+      <p onClick={props.click}>I'm {props.name} and I am {props.age}  years old!!</p>
       <p>{props.children}</p>
     </div>
   )
-}
+};
 
 export default person;
