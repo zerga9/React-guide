ce8c51cf966111e1b2157ee9910a8252cdb2d15a
Gadiza Zerari <gadizaz@hotmail.com>
2018-09-06 14:34:56 +0100

    Rendering content after clicking button

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
       { name: "Gadiza", age: 43 },
       { name: "Manu", age: 44 },
       { name: "Stephanie", age: 45 },
-    ]
+    ],
+    showPersons: false
   }
 
   switchNameHandler = (newName) => {
@@ -31,6 +32,11 @@ class App extends Component {
   })
   }
 
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
+  }
+
   render() {
     const style = {
       backgroundColor: 'white',
@@ -45,7 +51,9 @@ class App extends Component {
         <p>This is really working</p>
         <button
         style={style}
-        onClick={ () => this.switchNameHandler('Maximillian!!')}>Switch Name</button>
+        onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        {this.state.showPersons === true ?
+          <div>
         <Person
         name={this.state.persons[0].name}
         age={this.state.persons[0].age} />
@@ -57,6 +65,7 @@ class App extends Component {
         <Person
         name={this.state.persons[2].name}
         age={this.state.persons[2].age}/>
+        </div> : null }
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'))
