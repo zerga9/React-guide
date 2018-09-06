7c878aaaa15d01bacd03a4302b54632bbc8773a3
Gadiza Zerari <gadizaz@hotmail.com>
2018-09-06 15:43:18 +0100

    Changing all the individual names implemented

--- src/App.js
+++ src/App.js
@@ -21,13 +21,20 @@ class App extends Component {
 
   }
 
-  nameChangedHandler = (event) => {
-    this.setState({persons: [
-      { name: 'Gadiza', age: 43 },
-      { name: event.target.value, age: 44 },
-      { name: "Stephanie", age: 44 },
-    ]
-  })
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id
+    })
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+    this.setState( {persons: persons} );
   }
 
   togglePersonsHandler = () => {
@@ -54,7 +61,8 @@ class App extends Component {
          key={person.id}
          click={() => this.deletePersonHandler(index)}
          name={person.name}
-         age={person.age} />
+         age={person.age}
+         changed={(event) => this.nameChangedHandler(event, person.id)} />
        })}
      </div>
    );
