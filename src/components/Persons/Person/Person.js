6440eaa2c56f268f4a78da3f1151ae9d30ad1578
Gadiza Zerari <gadizaz@hotmail.com>
2018-10-01 14:22:42 +0100

    Add HOC in Person.js

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import classes from './Person.css';
-
+import WithClass from '../../../hoc/WithClass';
 
 class Person extends Component {
   constructor(props) {
@@ -20,18 +20,18 @@ class Person extends Component {
     console.log('[UPDATE Persons.js] inside componentWillReceiveProps', nextProps);
   }
 
-  
+
 
 
   render(){
     console.log('[Person.js] inside render()')
 
   return (
-    <div className={classes.Person}>
+    <WithClass classes={classes.Person}>
       <p onClick={this.props.click}>I am {this.props.name} and I am {this.props.age} years old!!</p>
       <p>{this.props.children}</p>
       <input type='text' onChange={this.props.changed} value={this.props.name}/>
-  </div>)
+  </WithClass>)
 };
 }
 
